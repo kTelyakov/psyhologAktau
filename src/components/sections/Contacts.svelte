50c1a4e7f7872f3315c60402b8149bba31ac97cc
Konstantin <telyakovkostya.learn@gmail.com>
2021-03-25 01:57:45 +0300

    Main page added

--- src/components/sections/Contacts.svelte
+++ src/components/sections/Contacts.svelte
@@ -13,7 +13,7 @@ import Heading from '../Heading.svelte'
 
     <div class="contactsSection__content">
       <div class="contactsSection__description">
-        <ul>
+        <ul class="contactsSection__list">
           <li class="contactsSection__listItem">Адрес: Республика Казахстан, Мангистауская Область, г.Актау, 28А мкр., Бизнес центр "Тианде", рядом с "Казком банк"</li>
           <li class="contactsSection__listItem">ИП Карайчикова Г.Б.
             Свидетельство о государственной регистрации ИП № 0029064
@@ -22,6 +22,19 @@ import Heading from '../Heading.svelte'
           </li>
           <li class="contactsSection__listItem">E-mail: psyholog.aktau@gmail.com</li>
         </ul>
+
+        <!-- Иконки -->
+        <div class="contactsSection__socials">
+          <a class="contactsSection__socialsItem" href="https://www.instagram.com/psyholog_aktau/" target="_blank">
+            <img src="socials/instagram.png" alt="">
+          </a>
+          <a class="contactsSection__socialsItem" href="https://vk.com/psyholog_aktau" target="_blank">
+            <img src="socials/vk.png" alt="">
+          </a>
+          <a class="contactsSection__socialsItem" href="https://www.facebook.com/psiholog.aktau/" target="_blank">
+            <img src="socials/fb.png" alt="">
+          </a>
+        </div>
       </div>
       <div class="contactsSection__map">
         <img src="map.png" alt="Карта">
@@ -52,10 +65,29 @@ import Heading from '../Heading.svelte'
     display: flex;
     justify-content: center;
     align-items: center;
+    & > img {
+      max-width: 100%;
+    }
+  }
+  &__list {
+    margin-bottom: 20px;
   }
   &__listItem {
     border-bottom: 1px solid #eee;
     padding: 5px;
+  }
+  &__socials {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: row;
+  }
+  &__socialsItem {
+    & > img {
+      width: 30px;
+      height: 30px;
+      margin-right: 10px;
+    }
   }
 }  
 </style>
